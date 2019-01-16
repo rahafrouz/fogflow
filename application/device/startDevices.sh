395434067b6e0c49cb1d846b306aa564b7435e58
showersky <showersky@gmail.com>
2019-01-16 07:49:33 +0000

    add the script for setting up the fed4iot demo

--- application/device/startDevices.sh
+++ application/device/startDevices.sh
@@ -1,19 +1,29 @@
-cd ./powerpanel
-rm nohup.out
-nohup node powerpanel.js profile1.json & 
+cd powerpanel
+rm *.log
+
+node powerpanel.js profile1.json > 1.log & 
 echo "start powerpanel #1"
-nohup node powerpanel.js profile2.json & 
+sleep 2
+
+node powerpanel.js profile2.json > 2.log & 
 echo "start powerpanel #2"
-nohup node powerpanel.js profile3.json & 
+sleep 2
+
+node powerpanel.js profile3.json > 3.log & 
 echo "start powerpanel #3"
+sleep 2
+
 cd ../camera1
-rm nohup.out
-nohup python fakecamera.py &
+rm tmp.log
+python fakecamera.py > tmp.log   &
 echo "start camera #1" 
+sleep 2
+
 cd ../camera2
-rm nohup.out
-nohup python fakecamera.py & 
+rm tmp.log
+python fakecamera.py > tmp.log   & 
 echo "start camera #2"
+sleep 2
 
 cd ../
 pwd
